f10e94a904ad38668fff588eddc9d1c7be0f686c
Rispectech <ue208085.rishabhverma.it@gmail.com>
2023-01-27 17:22:25 +0530

    fix

--- src/Components/ShopContainer.tsx
+++ src/Components/ShopContainer.tsx
@@ -1,31 +1,32 @@
-import { Container, Grid, LinearProgress } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
-import { useAppContext } from '../context'
-import { CardItem } from './CardItem'
-import { Drawer } from '@material-ui/core'
-import Cart from './Cart'
-import { useEffect } from 'react'
+import { Container, Grid, LinearProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { useAppContext } from "../context";
+import { CardItem } from "./CardItem";
+import { Drawer } from "@material-ui/core";
+import Cart from "./Cart";
+import { useEffect } from "react";
 
 const useStyles = makeStyles({
   container: {
     marginTop: 24,
-    width: '100%',
+    width: "100%",
   },
-})
+});
 
 const ShopContainer = () => {
-  const classes = useStyles()
-  const value = useAppContext()
+  const classes = useStyles();
+  const value = useAppContext();
 
   useEffect(() => {
-    if (value?.cart) value.Loaded()
-  }, [value?.cart])
-  if (value?.isLoading) return <LinearProgress className={classes.container} />
+    console.log(value);
+    if (value?.cart) value.Loaded();
+  }, [value, value?.cart]);
+  if (value?.isLoading) return <LinearProgress className={classes.container} />;
   else
     return (
       <Container className={classes.container}>
         <Drawer
-          anchor='right'
+          anchor="right"
           open={value?.state.isCartOpen}
           onClose={() => value?.openCart()}
         >
@@ -36,18 +37,14 @@ const ShopContainer = () => {
             ? value.cart?.map((item) => {
                 return (
                   <Grid item lg={4} md={4} sm={6} key={item.id}>
-                    <CardItem
-                      {...item}
-                      key={item.id}
-                      AddToCart={value.AddToCart}
-                    />
+                    <CardItem {...item} key={item.id} AddToCart={value.AddToCart} />
                   </Grid>
-                )
+                );
               })
             : null}
         </Grid>
       </Container>
-    )
-}
+    );
+};
 
-export default ShopContainer
+export default ShopContainer;
